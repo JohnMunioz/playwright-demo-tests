74e5b6e6f2d9242da0614a4f02951d6c2479f9f1
John Munioz <jfmunoz.ingeniero@gmail.com>
2025-04-15 19:07:50 -0500

    Configuración de pruebas demo con Playwright y configuraciones basicas

---
+++ tests/example.spec.ts
@@ -0,0 +1,32 @@
+import { test, expect } from '@playwright/test';
+import { describe } from 'node:test';
+
+test.describe("Test Suite", async() => {
+  test('User go to Playright page', async ({ page }) => {
+    
+    await test.step('User go to url', async () => {
+      await page.goto('https://playwright.dev/java/');
+    });
+
+    await test.step("Then the user can see the page", async () => {
+      await expect(page).toHaveTitle(/Playwright/);
+    });
+  });
+
+  test('get started link', async ({ page }) => {
+
+    await test.step('User go to url', async () => {
+    await page.goto('https://playwright.dev/java/');
+    });
+
+    // Click the get started link.
+    await test.step('And the User Click the get started link', async () => {
+    await page.getByRole('link', { name: 'Get Started' }).click();
+    });
+
+    // Expect page to have a heading with the name of Installation.
+    await test.step('Then the User expects page to have a heading with the name of Installation', async () => {
+    await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+    });
+  });
+});
